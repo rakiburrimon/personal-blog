803ee48faa51fd776a22aecb72297e375a8aff63
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2023-03-18 00:50:59 +0600

    Laravel Debugger
    Laravel debugger and config

--- config/app.php
+++ config/app.php
@@ -186,6 +186,9 @@ return [
          * Package Service Providers...
          */
 
+        /* Laravel Debugbar */
+         Barryvdh\Debugbar\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -210,6 +213,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
     ])->toArray(),
 
 ];
